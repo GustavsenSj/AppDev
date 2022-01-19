326a99177e23def00e64dba2e2deceb007003096
SjurG <sjurgustavsen@gmail.com>
2022-01-19 20:55:28 +0100

    Exercise 2.3

---
+++ EXERCISE2.3/src/test/java/no/ntnu/EXERCISE23/ApplicationTests.java
@@ -0,0 +1,13 @@
+package no.ntnu.EXERCISE23;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
